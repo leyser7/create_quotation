c1bc5f4f88709d043624a5e719fe00b491c63327
Cristian Camilo Alzate Anzola <ccalzate@bancolombia.com.co>
2021-08-20 23:54:16 -0500

    add first version gui

--- src/tkinter_app.py
+++ src/tkinter_app.py
@@ -14,7 +14,11 @@ class tkinterApp(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
 
         self.title("Crear Cotizacion")
-        self.client_name = "Sin Definir"
+        self.client_name = "Sin definir"
+        self.description = "Sin descripcion general"
+        self.items_n = []
+        self.unit_values = []
+        self.items_desc = []
         # creating a container
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
@@ -27,7 +31,7 @@ class tkinterApp(tk.Tk):
 
         # iterating through a tuple consisting
         # of the different page layouts
-        for F in (StartPage, EnterArticle, Page2):
+        for F in (StartPage, EnterArticle, EndPage):
 
             frame = F(container, self)
 
@@ -45,6 +49,13 @@ class tkinterApp(tk.Tk):
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
+
+    def create_exit_button(self, frame):
+        exit_button = ttk.Button(frame, text="Salir", command=self.destroy)
+
+        # putting the button in its place by
+        # using pack
+        exit_button.grid(row=5, column=0, padx=10, pady=10, sticky="s")
 
 
 # first window frame startpage
@@ -74,6 +85,8 @@ class StartPage(tk.Frame):
 
         text_entry = ttk.Entry(self, width=30)
         text_entry.grid(row=1, column=1, padx=10)
+        # spacer1 = tk.Label(self, text="Texto")
+        # spacer1.grid(row=2, column=0, rowspan=3, columnspan=3)
         button1 = ttk.Button(
             self,
             text="Siguiente",
@@ -82,91 +95,122 @@ class StartPage(tk.Frame):
                 self.get_values(text_entry),
             ],
         )
-
-        # putting the button in its place by
-        # using grid
-        button1.grid(row=2, column=1, padx=10, pady=10)
-
-        # button to show frame 2 with text layout2
-        button2 = ttk.Button(self, text="Salir", command=controller.destroy)
-
-        # putting the button in its place by
-        # using grid
-        button2.grid(row=2, column=0, padx=10, pady=10)
+        button1.grid(row=5, column=2, padx=10, pady=10)
+        controller.create_exit_button(self)
 
     def get_values(self, text_entry):
         self.controller.client_name = text_entry.get()
 
 
-# second window frame page1
+# second window frame enter article
 class EnterArticle(tk.Frame):
     def __init__(self, parent, controller):
 
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Ingrese articulo", font=LARGEFONT)
-        label.grid(row=0, column=0, padx=10, pady=10)
-
-        # button to show frame 2 with text
-
+        self.controller = controller
+        # Header
+        self.n = 0
+        self.text = tk.StringVar()
+        self.text.set(f"Ingrese la informacion del articulo {self.n}")
+        label = ttk.Label(
+            self,
+            textvariable=self.text,
+            font=LARGEFONT,
+        )
+        label.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
+        # Unit value
+        vcmd = (
+            self.register(self.validate),
+            "%P",
+        )
+        label = ttk.Label(
+            self, text="Precio unitario del articulo:", font=MEDIUMFONT
+        )
+        label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        self.unit_value_entry = ttk.Entry(
+            self, width=30, validate="all", validatecommand=vcmd
+        )
+        self.unit_value_entry.grid(row=1, column=1, padx=10)
+        self.unit_value_entry.insert(0, 0)
+        # Item n
+        label = ttk.Label(self, text="Cantidad de articulos:", font=MEDIUMFONT)
+        label.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+        self.item_n_entry = ttk.Entry(
+            self, width=30, validate="all", validatecommand=vcmd
+        )
+        self.item_n_entry.grid(row=2, column=1, padx=10)
+        self.item_n_entry.insert(0, 0)
+        # Description
+        label = ttk.Label(
+            self, text="Descripcion del articulo:", font=MEDIUMFONT
+        )
+        label.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+        self.desc_text = tk.Text(self, width=30, height=3)
+        self.desc_text.grid(row=3, column=1, padx=10)
+        # Add item
         button1 = ttk.Button(
             self,
             text="Agregar mas articulos",
             command=lambda: [
                 controller.show_frame(EnterArticle),
-                self.get_values(text_entry),
+                self.get_values(),
             ],
         )
-
-        # putting the button in its place by
-        # using grid
-        button1.grid(row=2, column=1, padx=10, pady=10)
-
-        # button to show frame 2 with text layout2
-        button2 = ttk.Button(self, text="Salir", command=controller.destroy)
-
-        # putting the button in its place by
-        # using grid
-        button2.grid(row=3, column=0, padx=10, pady=10)
-
-        button3 = ttk.Button(
+        button1.grid(row=4, column=1, padx=10, pady=10)
+        # Next button
+        button2 = ttk.Button(
             self,
-            text="Continuar",
-            command=lambda: controller.show_frame(Page2),
+            text="Siguiente",
+            command=lambda: [
+                controller.show_frame(EndPage),
+                self.get_values(),
+            ],
         )
+        button2.grid(row=5, column=1, padx=10, pady=10)
+        # Exit button
+        controller.create_exit_button(self)
 
-        # putting the button in its place by
-        # using grid
-        button3.grid(row=3, column=1, padx=10, pady=10)
+    def get_values(self):
+        self.controller.items_desc.append(self.desc_text.get("1.0", "end-1c"))
+        self.desc_text.delete("1.0", "end-1c")
+        self.controller.items_n.append(self.item_n_entry.get())
+        self.item_n_entry.delete(0, "end")
+        self.item_n_entry.insert(0, 0)
+        self.controller.unit_values.append(self.unit_value_entry.get())
+        self.unit_value_entry.delete(0, "end")
+        self.unit_value_entry.insert(0, 0)
+        self.n += 1
+        self.text.set(f"Ingrese la informacion del articulo {self.n}")
 
-    def get_values(self, text_entry):
-        self.controller.client_name = text_entry.get()
+    def validate(self, P):
+        if str.isdigit(P) or P == "":
+            return True
+        else:
+            return False
 
 
 # third window frame page2
-class Page2(tk.Frame):
+class EndPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page 2", font=LARGEFONT)
-        label.grid(row=0, column=0, padx=10, pady=10)
-
-        # button to show frame 2 with text
-        # layout2
-        button1 = ttk.Button(
-            self, text="Page 1", command=lambda: controller.show_frame(Page1)
-        )
-
-        # putting the button in its place by
-        # using grid
-        button1.grid(row=1, column=1, padx=10, pady=10)
-
-        # button to show frame 3 with text
-        # layout3
+        self.controller = controller
+        label = ttk.Label(self, text="Generar cotizacion", font=LARGEFONT)
+        label.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
+        # Description
+        label = ttk.Label(self, text="Descripcion general:", font=MEDIUMFONT)
+        label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        self.desc_text = tk.Text(self, width=30, height=3)
+        self.desc_text.grid(row=1, column=1, padx=10)
+        # Generate
         button2 = ttk.Button(
             self,
-            text="Startpage",
-            command=lambda: controller.show_frame(StartPage),
+            text="Generar",
+            command=lambda: [
+                self.get_values(),
+                controller.destroy(),
+            ],
         )
+        button2.grid(row=5, column=1, padx=10, pady=10)
 
-        # putting the button in its place by
-        # using grid
-        button2.grid(row=2, column=1, padx=10, pady=10)
+    def get_values(self):
+        self.controller.description = self.desc_text.get("1.0", "end-1c")
